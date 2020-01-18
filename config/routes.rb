1329877e2aade1f06e5fd396c677cc745ca97433
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-01-18 16:23:15 +0000

    finally working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
+  root 'tasks#index'
+
+  # # Read ALL tasks
+  # get 'tasks', to: 'tasks#index', as: :tasks
+
+  # # Create new restaurant (2 requests)
+  # # 'new' always needs to come before the 'show' - otherwise 'new' will be assisgned as ':id'
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+
+  # # Read ONE task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # Update a restaurant (2 requests)
+  # # First request - get the edit form
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :tasks_edit
+  # # Second request - update the record
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # Delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
